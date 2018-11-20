5a6ad39981876b03e95f74d4e530dd767ea530d1
rgarregrah <n94822437@gmail.com>
2018-11-20 16:27:54 +0900

    Introduction done

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
-function setup() {}
+function setup() {
+  //背景と設定
+  createCanvas(500, 300);
+  smooth();
+  background(230, 230, 230);
+  //2本の交差した線
+  stroke(130, 0, 0);
+  strokeWeight(4);
+  line(width / 2 - 70, height / 2 - 70, width / 2 + 70, height / 2 + 70);
+  line(width / 2 + 70, height / 2 - 70, width / 2 - 70, height / 2 + 70);
+  //円を描く
+  fill(255, 150);
+  ellipse(width / 2, height / 2, 50, 50);
+}
 
 function draw() {}
